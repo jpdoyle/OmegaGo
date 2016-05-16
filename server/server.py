bf23057a25fcfd33ccadf6522686fde9c337bb81
Joe Doyle (Ginto8) <ginto8@gmail.com>
2016-05-16 10:58:42 -0400

    host -> name

--- server/server.py
+++ server/server.py
@@ -101,7 +101,7 @@ def refreshWorker(wid,data):
         for w,dat in workers.iteritems():
             if now - dat['lastUpdate'] >= timedelta(seconds=30):
                 toRemove.append(w)
-            if dat['host'] == data['host'] and w != wid:
+            if dat['name'] == data['name'] and w != wid:
                 toRemove.append(w)
 
         for w in toRemove:
